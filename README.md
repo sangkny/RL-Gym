afeaa797b7030a7b33ef94c6636fa507e78b4573
sangkny <sangkny@yahoo.com>
2020-05-28 16:05:28 +0900

    updates

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # RL-Gym
+```angular2html
+Reinforce Learning Example based on Gym library
+
+```
+![cartpole-v1](./asset/CartPole-v1.png)
+
+Tested on Anaconda Envs (py36-pytorch-tf-gpu.yml)
+ - python 3.6
+ - tensorflow 1.10.0
+  
+  
+  ```angular2html
+files:
+ main.py : simple example in library, we can see how Gym libs work
+ RL-main.py : the first designed NN 
+```
---
+++ main.py
@@ -0,0 +1,17 @@
+# import librarys
+
+import gym
+import random
+
+env = gym.make('CartPole-v1')    # make an envirenment named 'Cartpole-v1' which has been defined in the library
+goal_steps = 100
+
+while True:
+    obs = env.reset() # reset the status
+    for i in range(goal_steps):
+        obs, reward, done, info = env.step(random.randrange(0,2)) # set the random number range which is the maximum numbers we can choose to make the bar uphold
+        # obs: object current status, reward: reward according to the performance, done: bar is out of range or falled down
+        if done:
+            break # escape the while loop if done
+
+        env.render() # render and display the environment
