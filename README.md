d9e753742e684efcd7fa895284194765c92056c7
sangkny <sangkny@yahoo.com>
2020-05-29 12:44:50 +0900

    updates

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # RL-Gym
-```angular2html
-Reinforce Learning Example based on Gym library
 
-```
 ![cartpole-v1](./asset/CartPole-v1.png)
 
 Tested on Anaconda Envs (py36-pytorch-tf-gpu.yml)
  - python 3.6
  - tensorflow 1.10.0
-  
+```angular2html
+Reinforce Learning Example based on Gym library
+- how to prepare the data
+- how to train the data
+- how to save the model
+- how to load the model and evaluate
+- how to predict the data 
+- how to improve the data-> now repeat the same procedures
+to do : pick the best score as the last model
+```
   
   ```angular2html
 files:
+ README.md : logs
  main.py : simple example in library, we can see how Gym libs work
  RL-main.py : the first designed NN 
 ```
--- RL-main.py
+++ RL-main.py
@@ -7,6 +7,8 @@
 import gym
 import numpy as np
 import random
+import os
+import time
 
 env = gym.make('CartPole-v1')
 
@@ -46,13 +48,14 @@ def data_preparation(N, K, f, render=False):
         print(training_set)
     return training_set
 
-from keras.models import Sequential
-from keras.layers import Dense
+from keras.models import Sequential, load_model, Model
+from keras.layers import Dense, Dropout
 from keras.optimizers import Adam
 def build_model():
   model = Sequential()
   model.add(Dense(128, input_dim=4, activation='relu'))
   model.add(Dense(52, activation='relu'))
+  model.add(Dropout(0.2))
   model.add(Dense(2, activation='softmax'))
   model.compile(loss='mse', optimizer=Adam())
   return model
@@ -64,27 +67,53 @@ def train_model(model, training_set):
 
 
 if __name__=='__main__':
-    N = 1000
+    N = 100
     K = 50
-
+    # training
     self_play_flag = True
     self_play_count = 10 # for better results
+    # model save/load model
+    ''' if you need saving weights please use .save_weights/load_weights
+        https://www.tensorflow.org/tutorials/keras/save_and_load?hl=ko
+    '''
+    load_model_flag = True # if not, start training from a scratch
+    load_saved_model = "./saved_models/rlmodel_1590723293.h5"   # latest model
 
-    model = build_model()
-    print('aqusiting data using randoem.randrange(0,2)...')
-    training_data = data_preparation(N=N, K=K, f=lambda s: random.randrange(0,2), render=True)
-    print('training model with training_data....')
-    train_model(model=model, training_set=training_data)
+    saved_model_flag = True
+    # build or load a model
+    if(load_model_flag):
+        if(os.path.isfile(load_saved_model)):
+            model = load_model(filepath=load_saved_model)
+    else:
+        model = build_model()
+
+    model.summary()                         # show the model summary
 
     def predictor(s):
         return np.random.choice([0,1], p=model.predict(s.reshape(-1,4))[0])
 
+    print('aqusiting data using randoem.randrange(0,2)...')
+    if(load_saved_model):
+        training_data = data_preparation(N=N, K=K, f=predictor, render=True)
+    else:
+        training_data = data_preparation(N=N, K=K, f=lambda s: random.randrange(0,2), render=True)
+
+    print('training model with training_data....')
+    train_model(model=model, training_set=training_data)
+
     if(self_play_flag):
         for i in range(self_play_count):
             K = (N//9+K)//2
-            print('iterative data_acquisition with model prediction... {0}/{1}..'.format(i,self_play_count))
+            print('iterative data_acquisition with model prediction... {0}/{1}..'.format(i,self_play_count-1))
             training_data = data_preparation(N=N, K=K, f=predictor, render=True)
             train_model(model=model, training_set=training_data)
 
-    print('final testing...')
+    if(saved_model_flag):
+        print('saving the final model...')
+        saved_model_path = "./saved_models/rlmodel_{}.h5".format(int(time.time()))
+        saved_model_dir = os.path.dirname(saved_model_path)
+        if(os.path.exists(saved_model_dir)==False):
+            os.makedirs(saved_model_dir)
+        model.save(saved_model_path)
+    print('final testing... evaluating ')
     data_preparation(100, 100, predictor, True)
